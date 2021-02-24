5793de24cdcbcd916705b395977c821b7ff9d32b
JoaoCarlosB <39831597+JoaoCarlosB@users.noreply.github.com>
2021-02-24 15:34:18 -0300

    W:GPG error: https://dl.winehq.org/wine-builds/ubuntu xenial InRelease: The following signatures couldn't be verified because the public key is not available: NO_PUBKEY 76F1A20FF987672F, E:The repository 'https://dl.winehq.org/wine-builds/ubuntu xenial InRelease' is not signed.    teste com novo arquivo
    v

---
+++ teste03.sh
@@ -0,0 +1,3 @@
+W:GPG error: https://dl.winehq.org/wine-builds/ubuntu xenial InRelease: The following signatures couldn't be verified because the public key is not available: NO_PUBKEY 76F1A20FF987672F, E:The repository 'https://dl.winehq.org/wine-builds/ubuntu xenial InRelease' is not signed.
+ 
+ teste com novo arquivo
